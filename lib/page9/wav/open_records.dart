2011d9acb08a31b555ae6340fec7412fade94227
ekdldksp123 <ekdldksp123@naver.com>
2021-09-02 21:50:53 +0900

    NOON-29 record tts and save as wav file
    Former-commit-id: 1abf3b47d468cbc005f7b20eb7e1e32453b0438c

--- lib/page9/wav/open_records.dart
+++ lib/page9/wav/open_records.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'record_and_save.dart';
-import 'record_test.dart';
 import '../../database/dto/voca.dart';
 import 'package:audio_service/audio_service.dart';
 
