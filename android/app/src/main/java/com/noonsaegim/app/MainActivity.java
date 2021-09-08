1a7e0c1c756598410c5d51e33b35b14b5ddd0cb6
ekdldksp123 <ekdldksp123@naver.com>
2021-09-08 16:40:17 +0900

    NOON-29 prepare for first deployment

--- android/app/src/main/java/com/noonsaegim/app/MainActivity.java
+++ android/app/src/main/java/com/noonsaegim/app/MainActivity.java
@@ -1,21 +1,7 @@
 package com.noonsaegim.app;
-import android.content.Context;
-import java.util.UUID;
-import java.io.File;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.speech.tts.TextToSpeech;
-import android.speech.tts.UtteranceProgressListener;
-import android.speech.tts.Voice;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-    private void synthesizeToFile(String text, String fileName) {
-        File file = new File(context.get)
-    }
 }
